8229054e9d37b21422a603d23eaa7ac873f0a931
Pawel Konior <pawel.konior@gmail.com>
2023-03-08 15:12:04 +0100

    add course function with permissions

--- courses/urls.py
+++ courses/urls.py
@@ -7,6 +7,7 @@ app_name = 'courses'
 urlpatterns = [
     path('', views.CourseListView.as_view(), name='courses-list'),
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course-detail'),
-    path('delete/<int:pk>', views.CourseDeleteView.as_view(), name='course-delete'),
+    path('delete/<int:pk>/', views.CourseDeleteView.as_view(), name='course-delete'),
+    path('create/', views.CourseCreateView.as_view(), name='course-create'),
 
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, DeleteView
+from django.views.generic import ListView, DetailView, DeleteView, CreateView
 
 from . import models
 from .permissions import OwnerRequiredMixin
@@ -19,9 +19,23 @@ class CourseDetailView(DetailView):
     context_object_name = 'course'
 
 
-class CourseDeleteView(LoginRequiredMixin, PermissionRequiredMixin,  OwnerRequiredMixin, DeleteView):
+class CourseDeleteView(LoginRequiredMixin,  OwnerRequiredMixin, DeleteView):
     model = models.Course
     success_url = reverse_lazy('courses:courses-list')
     login_url = reverse_lazy('users:login')
     template_name = 'courses/course_delete.html'
     context_object_name = 'course'
+
+
+class CourseCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    model = models.Course
+    fields = ('title', 'slug', 'subject', 'overview', 'course_image')
+    template_name = 'courses/course_create.html'
+    success_url = reverse_lazy('courses:courses-list')
+    login_url = reverse_lazy('users:login')
+    permission_required = 'courses.add_course'
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
