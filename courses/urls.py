022b381a6621e038698c97ecb1e7143e83807bee
Pawel Konior <pawel.konior@gmail.com>
2023-03-08 13:03:18 +0100

    add delete button

--- courses/urls.py
+++ courses/urls.py
@@ -6,5 +6,7 @@ app_name = 'courses'
 
 urlpatterns = [
     path('', views.CourseListView.as_view(), name='courses-list'),
+    path('<int:pk>/', views.CourseDetailView.as_view(), name='course-detail'),
+    path('delete/<int:pk>', views.CourseDeleteView.as_view(), name='course-delete'),
 
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,27 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, DeleteView
 
 from . import models
+from .permissions import OwnerRequiredMixin
 
 
 class CourseListView(ListView):
     model = models.Course
     template_name = 'courses/courses_list.html'
     context_object_name = 'courses'
+
+
+class CourseDetailView(DetailView):
+    model = models.Course
+    template_name = 'courses/course_details.html'
+    context_object_name = 'course'
+
+
+class CourseDeleteView(LoginRequiredMixin, PermissionRequiredMixin,  OwnerRequiredMixin, DeleteView):
+    model = models.Course
+    success_url = reverse_lazy('courses:courses-list')
+    login_url = reverse_lazy('users:login')
+    template_name = 'courses/course_delete.html'
+    context_object_name = 'course'
